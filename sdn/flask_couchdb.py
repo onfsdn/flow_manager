45eefabae42469cf456c0574e1efd48c25c70648
hariharaselvam <hariharaselvam8@gmail.com>
2016-07-03 09:33:10 +0530

    home page enhanced

--- sdn/flask_couchdb.py
+++ sdn/flask_couchdb.py
@@ -228,4 +228,4 @@ def logout():
 	return json.dumps(output)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug=True,host="0.0.0.0")
--- sdn/modules/sdn_home/sdn_home_controller.js
+++ sdn/modules/sdn_home/sdn_home_controller.js
@@ -2,6 +2,92 @@ window[appName].controller('sdn_home_controller',function($rootScope,$scope,$sta
 
 	console.log("Called");
 
+
+
+	//Constants for the SVG
+var width = 500,
+    height = 500;
+
+//Set up the colour scale
+var color = d3.scale.category20();
+
+//Set up the force layout
+var force = d3.layout.force()
+    .charge(-120)
+    .linkDistance(30)
+    .size([width, height]);
+
+//Append a SVG to the body of the html page. Assign this SVG as an object to svg
+var svg = d3.select("#visulization").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+//---Insert------
+//Set up tooltip
+var tip = d3.tip()
+    .attr('class', 'd3-tip')
+    .offset([-10, 0])
+    .html(function (d) {
+    return  d.name + "</span>";
+})
+svg.call(tip);
+//---End Insert---
+
+//Read the data from the mis element
+var mis = document.getElementById('mis').innerHTML;
+graph = JSON.parse(mis);
+
+//Creates the graph data structure out of the json data
+force.nodes(graph.nodes)
+    .links(graph.links)
+    .start();
+
+//Create all the line svgs but without locations yet
+var link = svg.selectAll(".link")
+    .data(graph.links)
+    .enter().append("line")
+    .attr("class", "link")
+    .style("stroke-width", function (d) {
+    return Math.sqrt(d.value);
+});
+
+//Do the same with the circles for the nodes - no
+var node = svg.selectAll(".node")
+    .data(graph.nodes)
+    .enter().append("circle")
+    .attr("class", "node")
+    .attr("r", 8)
+    .style("fill", function (d) {
+    return color(d.group);
+})
+    .call(force.drag)
+    .on('mouseover', tip.show) //Added
+    .on('mouseout', tip.hide); //Added
+
+//Now we are giving the SVGs co-ordinates - the force layout is generating the co-ordinates which this code is using to update the attributes of the SVG elements
+force.on("tick", function () {
+    link.attr("x1", function (d) {
+        return d.source.x;
+    })
+        .attr("y1", function (d) {
+        return d.source.y;
+    })
+        .attr("x2", function (d) {
+        return d.target.x;
+    })
+        .attr("y2", function (d) {
+        return d.target.y;
+    });
+
+    node.attr("cx", function (d) {
+        return d.x;
+    })
+        .attr("cy", function (d) {
+        return d.y;
+    });
+});
+
+
 	function HttpRequest(method,action, URL, parameter) {
 
 		$rootScope.showLoader = true;
@@ -41,7 +127,7 @@ window[appName].controller('sdn_home_controller',function($rootScope,$scope,$sta
 
 
 
-				$(function (response) {
+				/*$(function (response) {
 
 					//root = $scope.mesh_data
 					//update();
@@ -257,7 +343,7 @@ window[appName].controller('sdn_home_controller',function($rootScope,$scope,$sta
 
 					function TooltipContent(data){
 						console.log(data);
-						return "<table id='tooltip'><tr><td>"+data["APMAC"]+"<td></tr></table>"
+						return "<table id='tooltippop'><tr><td>"+data["APMAC"]+"<td></tr></table>"
 
 					}
 
@@ -277,7 +363,7 @@ window[appName].controller('sdn_home_controller',function($rootScope,$scope,$sta
 
 
 
-				});
+				});*/
 
 				break;
 
