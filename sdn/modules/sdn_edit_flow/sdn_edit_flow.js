7361bf1791c072bf8aef54136aa7f5c3156d1a02
hariharaselvam <hariharaselvam8@gmail.com>
2016-07-12 17:51:53 +0530

    update flow bug fix

--- sdn/modules/sdn_edit_flow/sdn_edit_flow.js
+++ sdn/modules/sdn_edit_flow/sdn_edit_flow.js
@@ -47,7 +47,7 @@ window[appName].controller('sdn_edit_flow_controller',function($rootScope,$scope
 				break;
 
             case 'get_flow_info':
-                $scope.flow = response.toString();
+                $scope.flow = JSON.stringify(response);
                 break;
 
 		}
