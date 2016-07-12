4d2217d505c7090b47860457bed0660063c240d5
hariharaselvam <hariharaselvam8@gmail.com>
2016-07-12 18:20:38 +0530

    Custome alert box

--- sdn/modules/sdn_register/sdn_register_controller.js
+++ sdn/modules/sdn_register/sdn_register_controller.js
@@ -1,66 +1,69 @@
-window[appName].controller('sdn_register_controller',function($rootScope,$scope,$state,$stateParams,$http,$window,$location,$q,$filter)	{
+window[appName].controller('sdn_register_controller', function ($rootScope, $scope, $state, $stateParams, $http, $window, $location, $q, $filter) {
 
 
-	if($scope.logged_in==undefined || $scope.logged_in=="")
-	{
-		window.location = "index.html";
-	}
+    if ($scope.logged_in == undefined || $scope.logged_in == "") {
+        window.location = "index.html";
+    }
 
-	function HttpRequest(method,action, URL, parameter) {
+    function HttpRequest(method, action, URL, parameter) {
 
-		$rootScope.showLoader = true;
+        $rootScope.showLoader = true;
 
-		var $promise = '';
-		if(method==="post") {
-			$promise = $http.post(URL, parameter);
-		} else {
-			$promise = $http.get(URL, parameter);
-		}
-		$promise.then(function (response) {
-			var result = angular.fromJson(response.data);
-			processTheData(action, result);
+        var $promise = '';
+        if (method === "post") {
+            $promise = $http.post(URL, parameter);
+        } else {
+            $promise = $http.get(URL, parameter);
+        }
+        $promise.then(function (response) {
+            var result = angular.fromJson(response.data);
+            processTheData(action, result);
 
-			$rootScope.showLoader = false;
+            $rootScope.showLoader = false;
 
-		});
-	};
+        });
+    };
 
-	function processTheData(action, response) {
+    function processTheData(action, response) {
 
-		switch (action) {
+        switch (action) {
 
 
-			case 'get_switch':
+            case 'get_switch':
 
-				$scope.switch_detail = response["rows"][1]["id"];
+                $scope.switch_detail = response["rows"][1]["id"];
 
-				break;
+                break;
 
-			case 'get_switch_info':
-				$scope.switch_info = response;
-				$scope.flow_info = response["rows"];
-				console.log($scope.flow_info);
-				break;
+            case 'get_switch_info':
+                $scope.switch_info = response;
+                $scope.flow_info = response["rows"];
+                console.log($scope.flow_info);
+                break;
 
-			case 'register_user':
-				$scope.register_info = response;
-				$scope.username = "";
-				$scope.password = "";
-				$scope.role = "";
-				alert("Successfully Created the User")
+            case 'register_user':
+                if (response["ok"]) {
+                    bootbox.alert("Flow added successfully!");
 
-				break;
-		}
 
-	}
+                }
+                else {
+                    bootbox.alert("Add Flow failed!");
+                }
+                console.log(response);
 
-    $scope.user_register = function() {
+                break;
+        }
+
+    }
+
+    $scope.user_register = function () {
 
         if ($scope.regsiter_user.isValid()) {
 
-			var data = {"username":$scope.username,"password":$scope.password,"role":$scope.role};
+            var data = {"username": $scope.username, "password": $scope.password, "role": $scope.role};
 
-			HttpRequest('post','register_user',window.flaskURL+'register_user',data);
+            HttpRequest('post', 'register_user', window.flaskURL + 'register_user', data);
 
 
         }
--- sdn/modules/sdn_user_list/sdn_user_list_controller.js
+++ sdn/modules/sdn_user_list/sdn_user_list_controller.js
@@ -1,33 +1,32 @@
-window[appName].controller('sdn_user_list_controller',function($rootScope,$scope,$state,$stateParams,$http,$window,$location,$q,$filter)	{
+window[appName].controller('sdn_user_list_controller', function ($rootScope, $scope, $state, $stateParams, $http, $window, $location, $q, $filter) {
 
-	console.log("Called");
-	if($scope.logged_in==undefined || $scope.logged_in=="")
-	{
-		window.location = "index.html";
-	}
+    console.log("Called");
+    if ($scope.logged_in == undefined || $scope.logged_in == "") {
+        window.location = "index.html";
+    }
 
-	function HttpRequest(method,action, URL, parameter) {
+    function HttpRequest(method, action, URL, parameter) {
 
-		$rootScope.showLoader = true;
+        $rootScope.showLoader = true;
 
-		var $promise = '';
-		if(method==="post") {
-			$promise = $http.post(URL, parameter);
-		} else {
-			$promise = $http.get(URL, parameter);
-		}
-		$promise.then(function (response) {
-			var result = angular.fromJson(response.data);
-			processTheData(action, result);
+        var $promise = '';
+        if (method === "post") {
+            $promise = $http.post(URL, parameter);
+        } else {
+            $promise = $http.get(URL, parameter);
+        }
+        $promise.then(function (response) {
+            var result = angular.fromJson(response.data);
+            processTheData(action, result);
 
-			$rootScope.showLoader = false;
+            $rootScope.showLoader = false;
 
-		});
-	};
+        });
+    };
 
-	function processTheData(action, response) {
+    function processTheData(action, response) {
 
-		switch (action) {
+        switch (action) {
 
             case 'get_all_users':
 
@@ -39,48 +38,54 @@ window[appName].controller('sdn_user_list_controller',function($rootScope,$scope
 
                 break;
 
-			case 'get_switch_info':
-				$scope.switch_info = response;
-				$scope.flow_info = response["rows"][1]["value"]["data"]["flows"];
-				break;
+            case 'get_switch_info':
+                $scope.switch_info = response;
+                $scope.flow_info = response["rows"][1]["value"]["data"]["flows"];
+                break;
 
-			case 'delete_flow':
+            case 'delete_flow':
 
-				$scope.del_info = response;
-				HttpRequest('get','get_all_users',window.flaskURL+'get_all_users','');
-				alert("Successfully Deleted")
-				break;
-
-            case 'delete_user':
-
-                $scope.del1_info = response;
-                HttpRequest('get','get_all_users',window.flaskURL+'get_all_users','');
+                $scope.del_info = response;
+                HttpRequest('get', 'get_all_users', window.flaskURL + 'get_all_users', '');
                 alert("Successfully Deleted")
                 break;
 
-		}
+            case 'delete_user':
 
-	}
+                if (response["ok"]) {
+                    bootbox.alert("User deleted successfully!");
 
 
-	$scope.flow_delete = function(id,rev) {
+                }
+                else {
+                    bootbox.alert("Delete user failed!");
+                }
+                console.log(response);
+                break;
 
-		var param = {"cancel":true};
+        }
 
-		bootbox.confirm("Are you sure you want to delete the user" , function (result) {
+    }
+
+
+    $scope.flow_delete = function (id, rev) {
+
+        var param = {"cancel": true};
+
+        bootbox.confirm("Are you sure you want to delete the user", function (result) {
             if (result) {
-				param = {"id":id,"rev":rev};
-                HttpRequest('post','delete_user',window.flaskURL+'delete_user',param);
+                param = {"id": id, "rev": rev};
+                HttpRequest('post', 'delete_user', window.flaskURL + 'delete_user', param);
             }
         });
 
-	}
+    }
 
-	//HttpRequest('get','getdocs',window.flaskURL+'getdocs','');
+    //HttpRequest('get','getdocs',window.flaskURL+'getdocs','');
 
-	//HttpRequest('get','get_switch',window.flaskURL+'get_switch','');
+    //HttpRequest('get','get_switch',window.flaskURL+'get_switch','');
 
-	HttpRequest('get','get_all_users',window.flaskURL+'get_all_users','');
-	//HttpRequest('get','get_all_flows',window.flaskURL+'get_all_flows','');
+    HttpRequest('get', 'get_all_users', window.flaskURL + 'get_all_users', '');
+    //HttpRequest('get','get_all_flows',window.flaskURL+'get_all_flows','');
 
 });
